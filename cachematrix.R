013e068af25e4e38106dc23b2f71380c77716a2f
corr62004 <corr681@gmail.com>
2017-01-15 20:48:34 +0900

    R programming- week3 assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCachematrix sets or reads a matrix or inverse of a matrix
+## Cachesolve returns inverse of matrix created with makeCachematrix if it exists
+## otherwise cachesolve calculates the inverse of a matrix
 
-## Write a short comment describing this function
+
+## Creates a matrix object whose original or inverse can be retrieved or set
+## x$get will tell you current value of matrix; x$set will set value of matrix x
+## x$getinv: return current value of x's inverse; x$setinv will invert x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ 
+  invmatrix = NULL
+  set = function(y) {
+    x <<- y
+    invmatrix <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) invmatrix <<- inverse 
+  getinv = function() invmatrix
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+
+## Outputs inverse of a special matrix if it is not already in cache
+## input is a matrix created using makeCacheMatrix prior to using cacheSolve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  cachedinv = x$getinv()
+  
+  # get inverse from cache if it already exists
+  if (!is.null(cachedinv)){
+    message("retrieving inverse from cache")
+    return(cachedinv)
+  }
+  
+  # else, invert the passed matrix x
+  passedmatrix = x$get()
+  cachedinv = solve(passedmatrix, ...)
+  
+  x$setinv(cachedinv)
+  
+  # return inverse of matrix x
+  return(cachedinv)
 }
